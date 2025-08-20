1a5c31ab5a3b7ce93fe6d72d9b69967e64512a65
stanly1br <stanlyalmeidabr@gmail.com>
2025-08-20 10:10:37 -0300

    Inplementação do casos 11 e 12

--- src/main/java/br/com/SmartMed/consultas/config/SegurityConfig.java
+++ src/main/java/br/com/SmartMed/consultas/config/SegurityConfig.java
@@ -13,7 +13,7 @@ public class SegurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Usa a nova sintaxe para desabilitar o CSRF
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll()
                 )
